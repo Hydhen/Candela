f0d19a6cf0e0b5218b9df5f0907a80e17dc2cea1
Loïc Juillet <juillet.loic@gmail.com>
2017-12-14 13:53:27 +0100

    First commit

---
+++ Assets/Scripts/GUI/LevelPanel.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class LevelPanel : MonoBehaviour {
+
+    public Text CurrentLevelText = null;
+
+    public TopCamera TC = null;
+
+    public void OnLevelButtonPressed()
+    {
+        // Set new Level Value
+        int lastLevel = LevelManager.Instance.GetCurrentLevel();
+        LevelManager.Instance.SetCurrentLevel(int.Parse(EventSystem.current.currentSelectedGameObject.name));
+
+        // Set UI Text Accordingly
+        CurrentLevelText.text = "Current Level: " + EventSystem.current.currentSelectedGameObject.name;
+
+        // Raise the TopCamera
+        Vector3 newOffset = new Vector3(0, (LevelManager.Instance.GetCurrentLevel() - lastLevel) * 5, 0);
+        TC.SetTargetOffset(newOffset);
+    }
+
+
+    private void Awake()
+    {
+        if (CurrentLevelText == null)
+        {
+            Debug.LogError("<color='Red'>No Current Level Text given</color>");
+        }
+
+        if (TC == null)
+        {
+            Debug.LogError("<color='Red'>No Camera given</color>");
+        }
+    }
+
+    private void Start()
+    {
+        if (CurrentLevelText)
+        {
+            CurrentLevelText.text = "Current Level: " + LevelManager.Instance.GetCurrentLevel().ToString();
+        }
+    }
+
+}
---
+++ Assets/Scripts/GameManagement/LevelManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    public static LevelManager Instance = null;
+
+    public GameObject[] Levels = null;
+
+    public string WallsParentObjectName = null;
+
+
+    private int CurrentLevel = 0;
+
+
+    public int GetCurrentLevel()
+    {
+        return CurrentLevel;
+    }
+
+    public void SetCurrentLevel(int level)
+    {     
+        foreach (Transform trans in Levels[CurrentLevel].transform)
+        {
+            trans.gameObject.layer = LayerMask.NameToLayer("Walls Disabled");
+        }
+
+        CurrentLevel = level - 1;
+
+        foreach (Transform trans in Levels[CurrentLevel].transform)
+        {
+            trans.gameObject.layer = LayerMask.NameToLayer("Walls Enabled");
+        }
+    }
+
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+
+        else if (Instance != this)
+            Destroy(gameObject);
+
+        if (WallsParentObjectName == null)
+        {
+            Debug.LogError("<color='Red'>No Walls Parent Object Name given</color>");
+        }
+
+        GameObject levelProps = GameObject.Find(WallsParentObjectName);
+        Transform[] children = levelProps.GetComponentsInChildren<Transform>();
+
+        int numberOfChildren = 0;
+
+        foreach (Transform child in children)
+        {
+            if (child.parent == levelProps.transform)
+            {
+                numberOfChildren++;
+            }
+        }
+
+        Levels = new GameObject[numberOfChildren];
+
+        foreach (Transform child in children)
+        {
+            if (child.parent == levelProps.transform)
+            {
+                Levels[int.Parse(child.name) - 1] = child.gameObject;
+            }
+        }
+    }
+
+}
