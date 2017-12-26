777966e8b2708d448da57d5e0a0dbb5223dd4683
Loïc Juillet <juillet.loic@gmail.com>
2017-12-26 18:45:43 +0100

    First test for camera

---
+++ Assets/Scripts/Camera/ThirdPersonCamera.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+[RequireComponent(typeof(Camera))]
+public class ThirdPersonCamera : MonoBehaviour {
+
+    [Tooltip("Target")]
+    public GameObject Target = null;
+    [Tooltip("Rotation Speed")]
+    public float RotationSpeed = 5f;
+    //Rewired
+    public int PlayerId = 0;
+
+
+    private Camera Cam = null;
+    private Vector3 Offset = Vector3.zero;
+    private float ShoulderOffset = 0;
+    private bool LeftShoulder = true;
+    //Rewired
+    private Player Player = null;
+
+
+	private void Awake()
+    {
+        Cam = GetComponent<Camera>();
+        Player = ReInput.players.GetPlayer(PlayerId);
+	}
+
+    private void Start()
+    {
+        Offset = Target.transform.position - transform.position;
+        ShoulderOffset = Offset.x;
+    }
+
+    private void UpdatePosition()
+    {
+        float currentAngle = transform.eulerAngles.y;
+        float desiredAngle = Target.transform.eulerAngles.y;
+        float angle = Mathf.LerpAngle(currentAngle, desiredAngle, Time.deltaTime * RotationSpeed);
+
+        Quaternion rotation = Quaternion.Euler(0, angle, 0);
+
+        transform.position = Vector3.Lerp(transform.position,
+            Target.transform.position - (rotation * Offset) + (!LeftShoulder ? Target.transform.right * (ShoulderOffset * 2) : Vector3.zero),
+            Time.deltaTime * RotationSpeed);
+    }
+
+    private void UpdateLook()
+    {
+        Quaternion rotation = Target.transform.rotation;
+        transform.rotation = rotation;
+        //transform.LookAt(Target.transform);
+    }
+
+    private void Update()
+    {
+        if (Player.GetButtonDown("Shoulder"))
+        {
+            LeftShoulder = !LeftShoulder;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        UpdatePosition();
+        UpdateLook();
+	}
+}
---
+++ Assets/Scripts/Camera/TopCamera.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopCamera : MonoBehaviour {
+
+    public Transform StartTarget = null;
+
+    public Vector3 PositionOffset = new Vector3(0, 0, 5);
+
+    public float Speed = 5f;
+
+
+    private Vector3 Target = Vector3.zero;
+
+
+    public Vector3 GetTarget()
+    {
+        return Target;
+    }
+
+    public void SetTarget(Vector3 position)
+    {
+        Target = position;
+    }
+
+    public void SetTargetOffset(Vector3 offset)
+    {
+        Target += offset;
+    }
+
+
+    private void Awake()
+    {
+        if (StartTarget)
+        {
+            Target = StartTarget.position;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (Target != null)
+        {
+            // Get new position
+            Vector3 newPosition = Target + PositionOffset;
+
+            // Lerp to this position
+            transform.position = Vector3.Lerp(transform.position, newPosition, Speed * Time.fixedDeltaTime);
+        }
+    }
+
+}
